26b587df861b9b38b141b890694fb487b2c2cfa8
wksnders <42388072+wksnders@users.noreply.github.com>
2025-09-01 17:23:25 -0600

    add 1 korean entry and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,24 @@
 <script setup>
-import useLocalization from '../composables/useLocalization'
-
-const { getTranslationRef,ready} = useLocalization()
-const headerText = getTranslationRef('brandPageTitle')
+import Navigation from './Navigation.vue';
 </script>
 
 <template>
+    <div class="footer-container">
+        <div class="border"></div>
+        <Navigation/>
+    </div>
     
 </template>
 
 <style scoped>
+.footer-container{
+    padding: 1rem;
+    width: 100%;
+}
+
+.border{
+    width: 100%;
+    height: 1px;
+    border-top: 1px solid var(--color-text);
+}
 </style>
