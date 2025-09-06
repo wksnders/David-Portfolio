757ae9d03bdc57b20d59a0541b8aeece58bec8de
wksnders <42388072+wksnders@users.noreply.github.com>
2025-09-06 10:28:13 -0600

    single pic on mobile

--- src/components/IntroductionImages.vue
+++ src/components/IntroductionImages.vue
@@ -3,7 +3,7 @@
     <div class="image-wrapper">
       <img src="/images/David1.jpg" alt="Image of David" />
     </div>
-    <div class="image-wrapper">
+    <div class="image-wrapper second-image">
       <img src="/images/David1.jpg" alt="Image of David" />
     </div>
   </section>
@@ -13,19 +13,21 @@
 .image-section {
   width: 100%;
   display: flex;
-  flex-direction: column; /* stack by default */
+  flex-direction: column; 
   gap: 1rem;
   padding: 0 10%;
   box-sizing: border-box;
 }
 
-/* Image container */
 .image-wrapper {
   width: 100%;
   aspect-ratio: 1;
 }
 
-/* Image fills container */
+.second-image {
+  display: none;
+}
+
 .image-wrapper img {
   width: 100%;
   height: 100%;
@@ -33,7 +35,6 @@
   border-radius: 8px;
 }
 
-/* Side-by-side for wider screens (>=800px) */
 @media (min-width: 800px) {
   .image-section {
     flex-direction: row;
@@ -41,7 +42,12 @@
   }
 
   .image-wrapper {
-    width: 50%; /* each takes half of available width */
+    width: 50%; 
+  }
+
+
+  .second-image {
+    display: block;
   }
 }
 </style>
